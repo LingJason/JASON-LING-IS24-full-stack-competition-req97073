345d7986405fcf0ce43b17d77d2526e950f174cb
Jason Ling <jasonling1996@gmail.com>
2023-03-31 09:39:01 -0700

    Create addProduct function with 6 digital unique generator

---
+++ server/routes/productRoutes.js
@@ -0,0 +1,13 @@
+const {mockData} = require("../mock/mockData");
+
+// Create a 6 digit random unique number and assigned it as a key to newProduct
+const addProduct = (newProduct) => {
+  const id = Math.floor(100000 + Math.random() * 900000).toString();
+  mockData[id] = newProduct;
+  return id;
+};
+
+
+module.exports = {
+  addProduct
+};
