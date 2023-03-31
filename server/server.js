40ff95dbbf493a112b33c816a9e78d1aa3582a38
Jason Ling <jasonling1996@gmail.com>
2023-03-31 09:33:55 -0700

    Create declarations, middleware setup, routes

---
+++ server/server.js
@@ -0,0 +1,22 @@
+// declarations
+const PORT = 3000;
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+//middleware setup
+app.use(bodyParser.json());
+app.use(cors());
+
+//routes import
+const productRouter = require("./routes/productRoutes.js");
+
+//routes
+app.use("/api/product", productRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
