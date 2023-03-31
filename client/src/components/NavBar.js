bc69f990236bd0b2f80a8606db59b2a58f2f4379
Jason Ling <jasonling1996@gmail.com>
2023-03-31 10:05:29 -0700

    Create navbar with logo that returns back to home page

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../NavBar.css";
+
+export default function NavBar() {
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+        <img src="https://www2.gov.bc.ca/StaticWebResources/static/gov3/images/gov_bc_logo.svg" alt="BC Service" width="150" height="75"/>
+        </Link>
+      </div>
+    </nav>
+  );
+}
