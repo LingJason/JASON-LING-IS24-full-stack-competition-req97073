c53ba8b8cdf1fd0db762932d5bf5c48294725a17
Jason Ling <jasonling1996@gmail.com>
2023-03-31 09:47:47 -0700

    Create a post request

---
+++ server/data/service/productService.js
@@ -0,0 +1,57 @@
+const {mockData} = require("../mock/mockData");
+
+// Create a 6 digit random unique number and assigned it as a key to newProduct
+const addProduct = (newProduct) => {
+  const id = Math.floor(100000 + Math.random() * 900000).toString();
+  mockData[id] = newProduct;
+  return id;
+};
+
+// Returns all mockData
+const getAllProducts = () => {
+  return mockData;
+};
+
+// Returns all data that matches search for scrum master
+const getProductByScrumMaster = (query) => {
+  const filteredProducts = {};
+  for (const [k, v] of Object.entries(mockData)) {
+    if (v.scrumMasterName.toLowerCase().includes(query.toLowerCase())) {
+      filteredProducts[k] = v;
+    }
+  }
+  return filteredProducts;
+};
+
+// Returns all data that matches search for developer
+const getProductByDeveloper = (query) => {
+  const filteredProducts = {};
+  for (const [k, v] of Object.entries(mockData)) {
+    if (v.developers.find(developer => developer.toLowerCase().includes(query.toLowerCase()))) {
+      filteredProducts[k] = v;
+    }
+  }
+  return filteredProducts;
+};
+
+// Update existing product
+const updateProduct = (id, updateProduct) => {
+  mockData[id] = {...mockData[id], ...updateProduct};
+  return mockData[id];
+};
+
+// Delete product
+const deleteProduct = (productId) => {
+  delete mockData[productId];
+  return productId;
+};
+
+
+module.exports = {
+  addProduct,
+  getAllProducts,
+  getProductByScrumMaster,
+  getProductByDeveloper,
+  updateProduct,
+  deleteProduct
+};
