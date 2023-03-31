1744af58caf65af28cb66b76be3c689fbb329a77
Jason Ling <jasonling1996@gmail.com>
2023-03-31 09:36:01 -0700

    Create 5 mock data examples

---
+++ server/data/mock/mockData.js
@@ -0,0 +1,43 @@
+const mockData = {
+  '674573': {
+    "scrumMasterName": "Woodman Piburn",
+    "productOwnerName": "Audrey Dandy",
+    "productName": "Y-find",
+    "methodology": "Waterfall",
+    "startDate": new Date("2022-09-25"),
+    "developers": ['Jason', 'Lee']
+  }, 
+  '986527': {
+    "scrumMasterName": "Thedric Rockwell",
+    "productOwnerName": "Frederique Allerton",
+    "productName": "Wrapsafe",
+    "methodology": "Agile",
+    "startDate": new Date("2022-11-30"),
+    "developers": ['Bruce', 'Calvin']
+  }, 
+  '245678': {
+    "scrumMasterName": "Ambrose Kennham",
+    "productOwnerName": "Editha Thomasen",
+    "productName": "Fixflex",
+    "methodology": "Agile",
+    "startDate": new Date("2022-03-22"),
+    "developers": ['Jenny', 'Ryan', 'Smith']
+  }, 
+  '862950': {
+    "scrumMasterName": "Letta Tucknutt",
+    "productOwnerName": "Cointon Islip",
+    "productName": "Zaam-Dox",
+    "methodology": "Waterfall",
+    "startDate": new Date("2023-01-07"),
+    "developers": ['Brock','Calvin']
+  }, 
+  '299575': {
+    "scrumMasterName": "Merline Oakenford",
+    "productOwnerName": "Mort Bengochea",
+    "productName": "Tresom",
+    "methodology": "Waterfall",
+    "startDate": new Date("2023-02-11"),
+    "developers": ['Tim', 'Larry']
+  }
+}
+module.exports = {mockData};
